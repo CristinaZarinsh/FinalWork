625ba2adb3704ad6796957fe503387ae65bc19f4
Cristina_Zarinsh <kot-199613@mail.ru>
2022-11-07 22:33:08 +0300

    Выполняем цикл  столько раз, сколько элементов в массиве, при этом проверяется условие: if (arrayStrings[i].Length <= length)

--- Program.cs
+++ Program.cs
@@ -32,3 +32,12 @@ for (int i = 0; i < size; i++)
 string[] arrayFinal = new string[size];
 int length = 3;
 int count = 0;
+
+for (int i = 0; i < size; i++)
+{
+    if (arrayStrings[i].Length <= length)
+    {
+        arrayFinal[count] = arrayStrings[i];
+        count++;
+    }
+}
